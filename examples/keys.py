40bd38b66e1bc09af196ab3d1ba82e9329aba98b
J. Chitty <jesuschitty7@hotmail.com>
2020-06-14 10:36:20 -0300

    Create keys.py
    Key generator and validator example

---
+++ examples/keys.py
@@ -0,0 +1,7 @@
+from ueosio import gen_key_pair, get_pub_key
+
+## Print Key Pair
+print(gen_key_pair())
+
+## Get Public Address from Private Key
+print(get_pub_key('5KJfEtwkMBp8t5HtgGfFZVdiUJhDejS5sbiE5siMVe1xs6RQa6y'))
