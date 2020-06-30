b3fc96b84d4339b390c6fdaaf6d46d598cbbfb81
J. Chitty <jesuschitty7@hotmail.com>
2020-06-30 11:42:17 -0300

    Update get_top_10_bps.py
    change kylin variable to node

--- examples/get_top_10_bps.py
+++ examples/get_top_10_bps.py
@@ -1,6 +1,6 @@
 from ueosio.rpc import Api
 
-kilin = Api("https://eos.greymass.com")
-for r in kilin.v1.chain.get_table_rows( code="eosio", scope="eosio", table="producers",
+node = Api("https://eos.greymass.com")
+for r in node.v1.chain.get_table_rows( code="eosio", scope="eosio", table="producers",
   key_type="float64", index_position="2", limit=10, json=True)["rows"]:
   print(r['owner'])
