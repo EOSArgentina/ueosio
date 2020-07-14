c827f62561e6b35c58ac04ad6fbed7e335407da8
J. Chitty <jesuschitty7@hotmail.com>
2020-07-14 12:41:07 -0300

    Update get_top_10_bps.py
    added different blockchain examples

--- examples/get_top_10_bps.py
+++ examples/get_top_10_bps.py
@@ -1,6 +1,14 @@
 from ueosio.rpc import Api
-
+  ## Change api endpoints for different eosio blockchains
+  
+# EOS Mainnnet
 node = Api("https://eos.greymass.com")
+for r in node.v1.chain.get_table_rows( code="eosio", scope="eosio", table="producers",
+  key_type="float64", index_position="2", limit=10, json=True)["rows"]:
+  print(r['owner'])
+  
+# WAX
+node = Api("https://api.wax.eosargentina.io")
 for r in node.v1.chain.get_table_rows( code="eosio", scope="eosio", table="producers",
   key_type="float64", index_position="2", limit=10, json=True)["rows"]:
   print(r['owner'])
